45c9efcb7af55a5580f072d788ac046364799d91
Nihal Londhe <79393628+nihallondhe@users.noreply.github.com>
2023-06-20 08:51:24 +0530

    Delete terraform directory

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "awsec2instance"{
-    description = "The public ip address of ec2 instance"
-    value = "${aws_instance.AWSEC2Instance.public_ip}
-}
-
-output "awsec2instance2"{
-    description = "The private ip address of ec2 instance"
-      value = "${aws_instance.AWSEC2Instance.private_ip}
-}
--- terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
- //access_key take from IAM role attach your IAM role to ec2 host machine
-}
